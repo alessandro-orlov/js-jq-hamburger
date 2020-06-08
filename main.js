b0850ffc3d2cd2c3c5fbfc9a0092e0ef7c3b3d4a
Alessandro <alex.ixs@hotmail.com>
2020-06-08 17:57:17 +0200

    FIX

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
 // Getting elements (Selettori sono lunghi per provare il funzionamento)
 var burgerMenuOpen = $('.header-right > a');
 var burgerMenuClose = $('.hamburger-menu a.close');
-var mediumDeviceMenu = $('.hamburger-menu')
+// var mediumDeviceMenu = $('.hamburger-menu') // ERRORE: var fuori dalla funzione
 
 // // Opens menu for medium and small device
-// $(burgerMenuOpen).click(
+// burgerMenuOpen.click(
 //   function() {
-//     $(mediumDeviceMenu).show(150);
+//     $('.hamburger-menu').show(150);
 //   }
 // );
 //
 // // Close menu for medium and small device
-// $(burgerMenuClose).click(
+// burgerMenuClose.click(
 //   function() {
-//     $(mediumDeviceMenu).hide(150);
+//     $('.hamburger-menu').hide(150);
 //   }
 // );
-//
+
 
 // ====================BONUS=====================
 // La stessa cosa con la classe esistente nel css
 
-$(burgerMenuOpen).click(
+burgerMenuOpen.click(
   function() {
-    $(mediumDeviceMenu).addClass('active');
+    $('.hamburger-menu').addClass('active');
   }
 );
 
-
 // Close menu for medium and small device
-$(burgerMenuClose).click(
+burgerMenuClose.click(
   function() {
-    $(mediumDeviceMenu).removeClass('active');
+    $('.hamburger-menu').removeClass('active');
   }
 );

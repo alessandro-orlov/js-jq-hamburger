cf3d30f8db0575bc485390af9ad34ad34a0409c9
Alessandro <alex.ixs@hotmail.com>
2020-06-08 14:17:37 +0200

    Esercizio completato

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert('ciao')
+// Getting elements (Selettori sono lunghi per provare il funzionamento)
+var burgerMenuOpen = $('.header-right > a');
+var burgerMenuClose = $('.hamburger-menu a.close');
+var mediumDeviceMenu = $('.hamburger-menu')
+
+// Opens menu for medium and small device
+$(burgerMenuOpen).click(
+  function() {
+    $(mediumDeviceMenu).show(150);
+  }
+);
+
+// Close menu for medium and small device
+$(burgerMenuClose).click(
+  function() {
+    $(mediumDeviceMenu).hide(150);
+  }
+)

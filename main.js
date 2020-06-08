c3b06375c35f93d4bd82f75c245c6991ebd821e5
Alessandro <alex.ixs@hotmail.com>
2020-06-08 14:57:49 +0200

    Trovato la classe active nel css :D

--- main.js
+++ main.js
@@ -3,16 +3,34 @@ var burgerMenuOpen = $('.header-right > a');
 var burgerMenuClose = $('.hamburger-menu a.close');
 var mediumDeviceMenu = $('.hamburger-menu')
 
-// Opens menu for medium and small device
+// // Opens menu for medium and small device
+// $(burgerMenuOpen).click(
+//   function() {
+//     $(mediumDeviceMenu).show(150);
+//   }
+// );
+//
+// // Close menu for medium and small device
+// $(burgerMenuClose).click(
+//   function() {
+//     $(mediumDeviceMenu).hide(150);
+//   }
+// );
+//
+
+// ====================BONUS=====================
+// La stessa cosa con la classe esistente nel css
+
 $(burgerMenuOpen).click(
   function() {
-    $(mediumDeviceMenu).show(150);
+    $(mediumDeviceMenu).addClass('active');
   }
 );
+
 
 // Close menu for medium and small device
 $(burgerMenuClose).click(
   function() {
-    $(mediumDeviceMenu).hide(150);
+    $(mediumDeviceMenu).removeClass('active');
   }
-)
+);
